1b97f186bc76e38f81d668519de24e85fe3f7696
Eganathan R <ammasamrith@gmail.com>
2025-07-25 14:02:33 +0530

    current user get call

--- catalyst/functions/CRUD/LinkyAPIService.java
+++ catalyst/functions/CRUD/LinkyAPIService.java
@@ -9,8 +9,8 @@ import com.zc.component.ZCUserDetail;
 import com.zc.component.users.ZCUser;
 import org.json.JSONObject;
 
-public class CRUD implements CatalystAdvancedIOHandler {
-    private static final Logger LOGGER = Logger.getLogger(CRUD.class.getName());
+public class LinkyAPIService implements CatalystAdvancedIOHandler {
+    private static final Logger LOGGER = Logger.getLogger(LinkyAPIService.class.getName());
 
     @Override
     public void runner(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -20,14 +20,19 @@ public class CRUD implements CatalystAdvancedIOHandler {
                     try {
                         ZCUserDetail currentUser = ZCUser.getInstance().getCurrentUser();
 
-                        JSONObject userJson = new JSONObject();
-//                        userJson.put("name","")
+                        JSONObject user = new JSONObject();
+                        user.put("user_id", currentUser.getUserId());
+                        user.put("name", currentUser.getFirstName() + " " + currentUser.getLastName());
+                        user.put("email", currentUser.getEmailId());
+                        user.put("created_time", currentUser.getCreatedTime());
+                        user.put("modified_time", currentUser.getModifiedTime());
 
+                        JSONObject data = new JSONObject();
+                        data.put("user", user);
 
                         JSONObject json = new JSONObject();
                         json.put("status", "success");
-                        json.put("user", "");
-
+                        json.put("data", data);
                         response.setStatus(200);
                         response.setContentType("application/json");
                         response.setCharacterEncoding("UTF-8");
